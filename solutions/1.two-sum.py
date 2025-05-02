8d02994a538cd90dc3fbd1e9d6b6b2709b82248c
Philippe Andrei <philippeandreid@gmail.com>
2025-05-02 17:36:33 +0800

    two sum, remove dup sorted arr

---
+++ solutions/1.two-sum.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=1 lang=python3
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    
+        nums_map = {}
+
+    
+        for i, num in enumerate(nums):
+
+            complement = target - num
+
+            if complement in nums_map:
+                return [nums_map[complement], i]
+            
+            nums_map[num] = i
+
+        return []
+# @lc code=end
+
