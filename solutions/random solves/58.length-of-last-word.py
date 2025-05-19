4eed3aefb8efff055ee05ee1ac2de0b80fb375fa
Philippe Andrei <philippeandreid@gmail.com>
2025-05-19 19:24:59 +0800

    random solve added

--- solutions/random solves/58.length-of-last-word.py
+++ solutions/random solves/58.length-of-last-word.py
@@ -8,11 +8,7 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
 
-
-        # MY SOLUTION
-        # 1. Strip the string of leading and trailing whitespace
-        # 2. Split the string into words using whitespace as the delimiter
-        # 3. Return the length of the last word
-        return len(s.strip().split()[-1])
+        return len(s.split()[-1])
+        
 # @lc code=end
 
